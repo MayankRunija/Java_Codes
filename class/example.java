ddbd4137f8c4701a1a875a932261f13d6a7f44c5
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2022-08-04 17:38:29 +0530

    Create example.java

---
+++ class/example.java
@@ -0,0 +1,17 @@
+class example{
+	public static void main(String args[]){
+		int i=10;
+		int j=10;
+		char ch = 'a'; 
+		int sum;
+		sum=i+j;
+		boolean b=true;
+		
+		System.out.println("Sum of Two Number is "+sum);
+		ch++;
+		System.out.println("alphabet come after a is "+ch);
+		System.out.println("I am the best : "+b);
+	
+	}
+
+}
