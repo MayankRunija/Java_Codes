59fd7c60af1ee9a2f044a487726ea3dcfe444ada
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2022-08-19 15:31:17 +0530

    Add files via upload

---
+++ box.java
@@ -0,0 +1,15 @@
+class Box{
+	int height,width,depth;
+	
+int volume(){
+	 int vol;
+	 vol= height*width*depth;
+ }
+}
+class Boxdemo{
+	public static void main(String args[]){
+	box b1 = new Box()
+	
+
+ }
+}
