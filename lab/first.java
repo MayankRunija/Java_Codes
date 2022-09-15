4a667e03efdfb8d9eb58be3f2fe79992f456b7d7
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2022-09-15 21:37:42 +0530

    Add files via upload

---
+++ lab/first.java
@@ -0,0 +1,11 @@
+class First{
+	public static void main(String arrgs[]){
+
+		System.out.println("Hello World!");
+		System.out.println("It's been nice knowing you");
+		System.out.println("Goodbye world!");
+
+ }
+
+
+}
