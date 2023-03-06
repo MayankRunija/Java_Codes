7a5d6b1c049f3c54f2ad66e2f611254eb7fc5f27
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2023-03-07 02:30:26 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,17 @@
+public class Typecasting {
+    public static void main(String[] args) {
+
+        char c = 'a';
+        int ch = c ;
+        System.out.println("Value of Char in int is :" + ch);
+
+        int a = (int)(45.3333) ;
+        System.out.println("integer value of a is :" + a);
+        int i = 257;
+        byte b = (byte)(i) ;
+        System.out.println("Byte value of B is :" + b);
+
+
+
+    }
+}
